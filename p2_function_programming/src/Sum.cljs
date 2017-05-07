5b459b96305ca7d8fa966fe69c7679af31cd7ad9
deadstone03 <deadstone03@gmail.com>
2017-05-07 16:12:12 -0700

    sum with reduce

--- p2_function_programming/src/Sum.cljs
+++ p2_function_programming/src/Sum.cljs
@@ -6,3 +6,10 @@
    (if (= (count alon) 0) 0 (+ (first alon) (sum (rest alon)))))
 
  (println (sum [1 2 3 4 5]))
+
+
+ (defn reduce-sum
+   "use reduce to caculate sum"
+   [alon]
+   (reduce (fn [s e] (+ s e)) 0 alon))
+ (println (reduce-sum [1 2 3 4 5]))
